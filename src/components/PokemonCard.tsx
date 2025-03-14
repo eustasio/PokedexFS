29ee4f3a422b3985e8db4f7ff6b2eae897097fb8
Florian Sebal <fsebal91@gmail.com>
2025-03-14 11:00:38 +0100

    ajout de données dans Pokemoncard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,31 @@
+const pokemonList = [
+    {
+        name: "bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+        imgSrc:"",
+    }
+];
+const pokelist = pokemonList.map;
 function PokemonCard() {
-    return(
-    <figure> 
-        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>Bulbizarre</figcaption>
-    </figure>
-    )
-}
-export default PokemonCard;
+    const pokemon = pokemonList[1]
+  
+    return (
+      <figure>
+        {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name}/>
+            ) : (
+                <p>???</p>
+            )
+        }
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+   )
+
+   
+  }
+  
+  export default PokemonCard;
